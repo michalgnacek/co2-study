6f8965ccca1780a8bb86c22deac91c0cc2bcf9ad
Michal Gnacek <michal.gnacek@emteq.net>
2023-04-09 18:06:36 +0100

    Added participant id and condition columns to synced participant data

--- classes/Participant.py
+++ classes/Participant.py
@@ -21,6 +21,8 @@ class Participant:
         self.co2_synced_data = pd.DataFrame()
         
     def set_air_mask_data(self, data):
+        data.insert(0, 'Participant_No', self.id)
+        data.insert(1, 'Condition', 'AIR')
         self.air_mask_data = data
         
     def set_air_eye_data(self, data):
@@ -33,6 +35,8 @@ class Participant:
         self.air_synced_data = data
         
     def set_co2_mask_data(self, data):
+        data.insert(0, 'Participant_No', self.id)
+        data.insert(1, 'Condition', 'CO2')
         self.co2_mask_data = data
         
     def set_co2_eye_data(self, data):
